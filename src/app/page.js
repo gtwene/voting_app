4f7196ef312cd347d53f6a86c7f4dd2348558587
Godfrey Twene <gtwene001@st.ug.edu.gh>
2023-05-01 17:28:20 +0000

    login

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Voting from '@/components/Vote/Voting'
+import Login from '@/components/auth/Login'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Voting />
+    <main className="flex flex-col justify-center items-center h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
+      {/* <Voting /> */}
+      <Login />
     </main>
   )
 }
