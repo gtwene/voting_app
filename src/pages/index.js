e204304cfa152b9b29b7b133877563fe66b7c284
Godfrey Twene <gtwene001@st.ug.edu.gh>
2023-05-01 21:54:34 +0000

    Elections Portal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-import Voting_Page from "@/components/Vote/Voting";
-import Votal_Portal from "@/components/Vote/Voting_portal";
-import Login from "@/components/auth/Login";
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Votal_Portal from './components/Vote/Voting_portal';
+import Login from './components/auth/Login';
+import VotingApp from './components/Vote/Voting';
 
 export default function Home() {
   return (
     <>
-      {/* <Login /> */}
-      {/* <Voting_Page /> */}
+      <Login />
+      <VotingApp />
       <Votal_Portal />
     </>
   );
